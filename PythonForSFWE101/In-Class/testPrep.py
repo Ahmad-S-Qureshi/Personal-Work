2378de175340b6a0eeaed1a75b06460b231be8ef
Ahmad-S-Qureshi <113152659+Ahmad-S-Qureshi@users.noreply.github.com>
2023-03-30 09:34:28 -0700

    Revert "final touches"
    This reverts commit 43a57212dcc8f6b52df09a89e68212a2c677cd52.

--- PythonForSFWE101/In-Class/testPrep.py
+++ PythonForSFWE101/In-Class/testPrep.py
@@ -1,28 +1,18 @@
 import math
 import random
-# def checker():
-#     booleanVals = [True, False]
-#     for a in booleanVals:
-#         for b in booleanVals:
-#             for y in booleanVals:
-#                 for x in booleanVals:
-#                     # Add tested vals to the if
-#                     boolVal1 = not x and y == a and b
-#                     boolVal2 = not ((x and (y == a)) and b)
-#                     if((boolVal1) != (boolVal2)):
-#                         print("Not Equal")
-#                         return
-#     print("Equal")
-# checker()
+def checker():
+    booleanVals = [True, False]
+    for a in booleanVals:
+        for b in booleanVals:
+            for y in booleanVals:
+                for x in booleanVals:
+                    # Add tested vals to the if
+                    boolVal1 = not x and y == a and b
+                    boolVal2 = not ((x and (y == a)) and b)
+                    if((boolVal1) != (boolVal2)):
+                        print("Not Equal")
+                        return
+    print("Equal")
+    return
 
-grades = { 'A': 90, 'B': 80, 'C': 70, 'D': 60 }
-my_grade = 70
-if my_grade not in grades:
-    z = 1
-else:
-    z = 2
-if 'F' in grades:
-    z = z + 10
-else:
-    z = z + 20
-print(z)
+checker()
